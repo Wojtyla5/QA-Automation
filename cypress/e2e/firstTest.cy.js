6ca7295864233c0b296f6b0e8725926470d6f897
Wojtyla5 <wh2025cod@interia.pl>
2024-04-02 22:16:04 +0200

    first commit

---
+++ cypress/e2e/firstTest.cy.js
@@ -0,0 +1,20 @@
+describe ("Pierwszy blok testów", () => {
+    
+    it ("Wpisz e-mail i hasło dla user888", () => {
+        cy.visit("https://www.edu.goit.global/account/login")
+        cy.login("user888@gmail.com", "1234567890")
+        cy.get('#open-navigation-menu-mobile').click()
+        cy.get(':nth-child(12) > .next-bve2vl').scrollIntoView().should("be.visible")
+        cy.get(':nth-child(12) > .next-bve2vl').click()
+    });
+
+    it ("Wpisz e-mail i hasło dla testowyqa", () => {
+        cy.visit("https://www.edu.goit.global/account/login")
+        cy.login("testowyqa@qa.team", "QA!automation-1")
+        cy.get('#open-navigation-menu-mobile').click()
+        cy.get(':nth-child(9) > .next-bve2vl').scrollIntoView().should("be.visible")
+        cy.get(':nth-child(9) > .next-bve2vl').click()
+    });
+
+
+});
